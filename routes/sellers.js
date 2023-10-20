50de265d7a7b3f88d51af9ae8da418f9ae0500a6
Walter <w.obadha@gmail.com>
2023-10-20 06:13:58 +0300

    build sellers api

--- routes/sellers.js
+++ routes/sellers.js
@@ -3,17 +3,55 @@ const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
 
-router.use("route");
-
 router.get("/", async (req, res) => {
   const sellers = await Seller.find();
   res.send(sellers);
 });
 
-router.post("/");
+router.post("/", async (req, res) => {
+  let seller = new Seller({
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+    address: req.body.address,
+  });
+  seller = await seller.save();
 
-router.put("/:id");
+  res.send(seller);
+});
 
-router.delete("/:id");
+router.put("/:id", async (req, res) => {
+  const seller = await Seller.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: req.body.name,
+      email: req.body.email,
+      phone: req.body.phone,
+      address: req.body.address,
+    },
+    { new: true }
+  );
 
-router.get("/:id");
+  if (!seller)
+    return res.status(400).send("Seller with given ID was not found");
+
+  res.send(seller);
+});
+
+router.delete("/:id", async (req, res) => {
+  const seller = await Seller.findByIdAndRemove(req.params.id);
+
+  if (!seller) return res.status(400).send("Seller with given ID not found.");
+
+  res.send(seller);
+});
+
+router.get("/:id", async (req, res) => {
+  const seller = await Seller.findById(req.params.id);
+
+  if (!seller) return res.status(404).send("Seller with given ID not found");
+
+  res.send(seller);
+});
+
+module.exports = router;
