b68d02f1f6a2b0a4db28452f5b5f7595c0e5e8a1
Walter <w.obadha@gmail.com>
2023-10-20 06:14:18 +0300

    build sellers api

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const sellers = require("./routes/sellers");
 const express = require("express");
 const app = express();
 
@@ -7,7 +8,8 @@ mongoose
   .then(() => console.log("Connected to MongoDb..."))
   .catch((err) => console.error("Could not connect to MongoDB..."));
 
-app.use(express.json);
+app.use(express.json());
+app.use("/api/sellers", sellers);
 
 const port = process.env.port || 3000;
-app.listen(port, () => console.log("Listening on port ${port} ..."));
+app.listen(port, () => console.log(`Listening on port ${port} ...`));
